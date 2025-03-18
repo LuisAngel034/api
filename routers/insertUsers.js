d65832ad7fca9f88a3674f50e6528814619865e5
ray <rayito0268@gmail.com>
2025-03-18 08:15:07 -0600

    prueba beta 1

--- routers/insertUsers.js
+++ routers/insertUsers.js
@@ -4,24 +4,24 @@ const User = require('../models/users5B.js');
 
 rutaInsertUsers.post('/api/insertUsers', async (req, res) => {
     try {
-        console.log('Datos recibidos:', req.body); // Depuración: Verifica los datos recibidos
-
         const nuevoUsuario = new User({
             nombre: req.body.nombre,
             Apaterno: req.body.Apaterno,
             Amaterno: req.body.Amaterno,
-            correo: req.body.correo,
             telefono: req.body.telefono,
-            rol: 'usuario', // Asignar "usuario" por defecto
+            correo: req.body.correo,
+            rol: 'usuario',
             "contraseña": req.body["contraseña"]
         });
 
         const usuarioGuardado = await nuevoUsuario.save();
-        console.log('Usuario guardado:', usuarioGuardado); // Depuración: Verifica el usuario guardado
-
         return res.status(201).json({ status: true, data: usuarioGuardado });
     } catch (err) {
-        console.error('Error al guardar el usuario:', err); // Depuración: Imprime el error
+        // Capturar el error de correo duplicado
+        if (err.code === 11000 && err.keyPattern.correo) {
+            return res.status(400).json({ status: false, message: 'El correo ya está registrado.' });
+        }
+        // Otros errores
         res.status(500).json({ status: false, message: err.message });
     }
 });
