dd8ec261fe8711e5431f41feb378299c29a9eab1
ray <rayito0268@gmail.com>
2025-03-19 12:03:45 -0600

    beta 4

--- routers/consults.js
+++ routers/consults.js
@@ -1,21 +1,21 @@
-const express=require('express');
-const ruta=express.Router();
-const Sensores=require('../models/senai.js');
+const express = require('express');
+const ruta = express.Router();
+const Sensores = require('../models/senai.js');
 
-ruta.post('/api/sensoresDatos', async (req,res)=>{
-    try{
-        console.log(req.body)
-        let sensor=await Sensores.findOne({numero_serie:req.body.numero_serie});
-        if(!sensor){
-            return res.status(200).json({message:'Usuario no encontrado', status:false});
-        }
-        return res.status(200).json({sensor, status:true});
-    }catch(err){
-        res.status(500).json({message:err.message});
+ruta.post('/api/sensoresDatos', async (req, res) => {
+  try {
+    console.log(req.body);
+    let sensor = await Sensores.findOne({ numero_serie: req.body.numero_serie });
+    if (!sensor) {
+      return res.status(200).json({ message: 'Invernadero no encontrado', status: false });
     }
+    return res.status(200).json({ sensor, status: true });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-module.exports=ruta
+module.exports = ruta;
 
 
 /*const express=require('express');
